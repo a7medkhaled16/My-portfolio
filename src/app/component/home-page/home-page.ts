8549b80b60a1bfc6763b2d73a2436752b316d153
Ahmed Aboelsoud <aboelsoudahmed345@gmail.com>
2026-01-03 04:22:21 +0200

    add cv button

--- src/app/component/home-page/home-page.ts
+++ src/app/component/home-page/home-page.ts
@@ -5,6 +5,7 @@ import {
   animate,
   transition
 } from '@angular/animations';
+
 import { AboutComponent } from "../about-component/about-component";
 import { EducationComponent } from "../education-component/education-component";
 import { ProjectComponent } from "../project-component/project-component";
@@ -19,18 +20,54 @@ import { TranslateModule } from '@ngx-translate/core';
     animations: [
     trigger('slideInLeft', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(-300px)' }),
+        style({ opacity: 0, transform: 'translateX(-100px)' }),
         animate('1s ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
       ])
     ]),
     trigger('slideInRight', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(300px)' }),
+        style({ opacity: 0, transform: 'translateX(100px)' }),
         animate('1s ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      ])
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1.5s ease-in', style({ opacity: 1 }))
       ])
     ])
   ]
 })
 export class HomePage {
 
+ displayText = '';
+  jobTitles = ['Frontend Developer', 'Angular Developer'];
+  currentIndex = 0;
+  isDeleting = false;
+
+  ngOnInit() {
+    this.typeWriter();
+  }
+
+  typeWriter() {
+    const current = this.jobTitles[this.currentIndex];
+    
+    if (this.isDeleting) {
+      this.displayText = current.substring(0, this.displayText.length - 1);
+    } else {
+      this.displayText = current.substring(0, this.displayText.length + 1);
+    }
+
+    let typeSpeed = this.isDeleting ? 50 : 150;
+
+    if (!this.isDeleting && this.displayText === current) {
+      typeSpeed = 2000;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.displayText === '') {
+      this.isDeleting = false;
+      this.currentIndex = (this.currentIndex + 1) % this.jobTitles.length;
+    }
+
+    setTimeout(() => this.typeWriter(), typeSpeed);
+  }
 }
